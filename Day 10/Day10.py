12c5ee8b0a18bd3640bb6baefc046e99792b3538
xlzior <wenjun.lye@gmail.com>
2021-12-30 13:14:19 +0800

    Extract out Point class

--- Day 10/Day10.py
+++ Day 10/Day10.py
@@ -3,40 +3,7 @@ import math
 import queue
 import sys
 from itertools import combinations
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-    def angle(self):
-        if self.x == 0:                          # edge cases: straight up and straight down
-            return 0 if self.y < 0 else math.pi
-        raw_angle = math.atan(-self.y / self.x)  # -self.y in order to account for y increasing downwards
-        correction = math.pi / 2 if self.x > 0 else 3 * math.pi / 2  # convert tangent info into 360 degrees rotation
-        return correction - raw_angle
-
-    def manhattan(self, other):
-        return abs(self.x - other.x) + abs(self.y - other.y)
-
-    def euclidean(self, other):
-        return math.sqrt(math.pow(self.x - other.x, 2) +
-                         math.pow(self.y - other.y, 2))
+from Point import Point
 
 class Asteroid:
     def __init__(self, location):
